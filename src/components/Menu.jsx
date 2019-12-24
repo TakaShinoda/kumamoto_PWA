37642926efd4f955bed3aedae7d34d7b5e41ef72
Takahiro <tkhr_621@docomo.ne.jp>
2019-12-25 03:19:58 +0900

    react-router-domの追加

---
+++ src/components/Menu.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import About from './About';
+
+
+class Menu extends Component {
+    liStyle = {
+        display: 'inline',
+        width: '100px'
+    }
+
+    render() {
+        return (
+            <div>
+            <Router>
+                <div style={{width: '500px', textAlign: 'left'}}>
+                    <ul style={{display: 'flex'}}>
+                        <li style={this.liStyle}><Link to='./'>top</Link></li>
+                        <li style={this.liStyle}><Link to ='./About'>About</Link></li>
+                    </ul>
+                    <hr />
+                    <Route exact path='/' component={Menu} />
+                    <Route  path='./About' component={About} />
+                </div>
+            </Router>
+
+            
+
+            
+            </div>
+        
+        );
+    }
+}
+
+export default Menu;
